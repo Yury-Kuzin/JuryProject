ce1e004493623f0c022abdbfb6a38a3980644980
Андрей Боев <a.boev.it@gmail.com>
2023-05-17 13:27:16 +0300

    Modify fill_matrix_2 function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Jury Project
 def fill_matrix_1(n, m):
+    # Comment 666
     return [[i * j for j in range(m)] for i in range(n)]
 
 
 def fill_matrix_2(n, m):
+    # Comment
     return [[(i + j) % 2 for j in range(m)] for i in range(n)]
